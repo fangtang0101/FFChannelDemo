a8b80a2a9e87e145eecfe126bc7b02b0cd5b1920
fangtang0101 <administrator@sudidemacbook-pro.local>
2016-11-14 14:20:42 +0800

    Initial Commit

---
+++ FFChannelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFChannelDemo
+//
+//  Created by Administrator on 16/11/14.
+//  Copyright © 2016年 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFChannelDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFChannelDemo
+//
+//  Created by Administrator on 16/11/14.
+//  Copyright © 2016年 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
